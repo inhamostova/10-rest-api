388223cc7f2ced3a8e21e42979c641f635f1f3c9
inhamostova <inha.mostova@gmail.com>
2026-07-08 20:02:41 +0300

    weather api

---
+++ src/js/01-weather.js
@@ -0,0 +1,56 @@
+const BASE_URL = 'http://api.weatherapi.com/v1';
+const API_KEY = '9d14047e46094293a1f155227260807';
+
+const cityTitle = document.querySelector('.city');
+const container = document.querySelector('.container');
+const searchForm = document.querySelector('.search-form');
+
+searchForm.addEventListener('submit', onSearchSubmit);
+
+function onSearchSubmit(evt) {
+  evt.preventDefault();
+
+  const {
+    city: { value: cityValue },
+    days: { value: daysValue },
+  } = evt.currentTarget.elements;
+
+  if (!cityValue) {
+    alert('Enter city');
+    return;
+  }
+
+  fetchWeather(cityValue, daysValue).then(data => renderMarkup(data));
+}
+
+function renderMarkup(data) {
+  cityTitle.textContent = `City: ${data.location.name}`;
+  container.innerHTML = createMarkup(data.forecast.forecastday);
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      item => `<li>
+            <h3>${item.date}</h3>
+            <img src="${item.day.condition.icon}" alt="${item.day.condition.text}">
+            <p>Temperature: ${item.day.avgtemp_c}&deg;C</p>
+            <p>${item.day.condition.text}</p>
+        </li>`
+    )
+    .join('');
+}
+
+function fetchWeather(city, days) {
+  return fetch(
+    `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}`
+  )
+    .then(resp => {
+      console.log(resp);
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
+    })
+    .catch(err => console.error(err));
+}
